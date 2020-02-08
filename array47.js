f9486a7d5d03a6fa9d9635c2a57278f72407718d
Jon Mockbee <mockbee1000@gmail.com>
2020-02-08 14:27:14 -0500

    all array work

---
+++ array47.js
@@ -0,0 +1,8 @@
+let topSongs = [
+    'First time I ever saw your face',
+    'God only knows ',
+    'A day in the life',
+    'Life on Mars'
+];
+
+topSongs.push ('fortunate son');
---
+++ array49.js
@@ -0,0 +1,5 @@
+let fruits = ['apple', 'banna',];
+let veggies = ['asparagus', 'brussel sprouts'];
+
+console.log (fruits.concat (veggies));
+
